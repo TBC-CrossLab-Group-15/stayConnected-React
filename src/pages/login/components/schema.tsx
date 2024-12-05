56756b12f758444625f4194b8bbef7bb1b56189c
Omiadze <teoomiadze18@gmail.com>
2024-12-05 23:46:21 +0400

    fix marker errors

--- src/pages/login/components/schema.tsx
+++ src/pages/login/components/schema.tsx
@@ -12,10 +12,6 @@ export const LoginFormSchema = z.object({
     .max(50, { message: "validation.password-max" })
     .regex(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-<<<<<<< HEAD
       { message: "validation.password-strength" }
-=======
-      { message: "validation.password-strength" }, // Custom message for password strength
->>>>>>> d54cc352ae486109689a3b2992c1ccc6c9f05133
     ),
 });
