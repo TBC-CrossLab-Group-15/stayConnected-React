058f0159509fd51d2a58571bf0f2e455e90db60d
giorgi <zautashvili1994@gmail.com>
2024-12-08 13:18:05 +0400

    add padding to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -36,7 +36,7 @@ const Header = () => {
   return (
     <div className="z-50 dark:bg-black  overflow-hidden  sticky top-0 left-0 w-full   bg-white shadow-[0px_-2px_4px_rgba(0,0,0,0.1)] border-solid border-b border-b-gray-300 dark:border-b-solid dark:border-b-neutral-800">
       <div className=" w-full sm:w-[90%]  mx-auto p-0  sm:px-5  h-20 flex items-center justify-between gap-4 ">
-        <div className="text-2xl font-bold  ">
+        <div className="text-2xl font-bold pl-5 ">
           <NavLink to="/">
             <svg
               xmlns="http://www.w3.org/2000/svg"
